faed2e9127273b428d845f5d4d692b26feef6304
caiwenhao <caiwenhao186@live.com>
2023-10-20 12:14:57 +0800

    Update workflows.py

--- workflows.py
+++ workflows.py
@@ -1,14 +1,48 @@
 from datetime import timedelta
 from temporalio import workflow
-from activities import send_email  # 引入活动
+from activity_registry import activity_registry
 
 @workflow.defn
-class NotifyOrder:
+class AbstractWorkflow:
     @workflow.run
-    async def run(self, amount: float) -> str:
-        if amount > 20:
-            return await workflow.execute_activity(
-                send_email, "admin@example.com",start_to_close_timeout=timedelta(seconds=5)
+    async def run(self, config):
+        trigger = config.get('trigger')
+        conditions = config.get('conditions', [])
+        actions = config.get('actions', [])
+        
+        if not self.check_trigger(trigger):
+            return "Trigger conditions not met."
+        
+        if not all(self.check_condition(c) for c in conditions):
+            return "Global conditions not met."
+        
+        for i, action in enumerate(actions):
+            action_conditions = action.get('pre_conditions', [])
+            if not all(self.check_condition(c) for c in action_conditions):
+                continue
+            
+            await self.execute_action(action)
+            
+            action_post_conditions = action.get('post_conditions', [])
+            if not all(self.check_condition(c) for c in action_post_conditions):
+                break
+
+    def check_trigger(self, trigger):
+        return True
+
+    def check_condition(self, condition):
+        return True
+
+    async def execute_action(self, action):
+        activity_type = action.get('activity')
+        parameters = action.get('parameters', {})
+        
+        activity_fn = activity_registry.get(activity_type)
+        if activity_fn:
+            await workflow.execute_activity(
+                activity_fn,
+                **parameters,
+                start_to_close_timeout=timedelta(seconds=5)
             )
         else:
-            return "Order amount too small, no email sent."
+            print(f"Activity {activity_type} not found.")
