ed52f178c310bb36c26d814f4eac2247cd88a6f3
caiwenhao <caiwenhao186@live.com>
2023-10-20 04:35:02 +0000

    1

--- run_worker.py
+++ run_worker.py
@@ -2,18 +2,16 @@ import asyncio
 from temporalio import workflow, activity
 from temporalio.client import Client
 from temporalio.worker import Worker
-from workflows import NotifyOrder  # 引入工作流
-from activities import send_email  # 引入活动
+from abstract_workflow import AbstractWorkflow  # 引入工作流
+import activities 
 
 async def main():
-    client = await Client.connect("10.43.12.80:7233", namespace="default")
-
+    client = await Client.connect("192.168.66.174:7233", namespace="default")
     # 初始化 worker
     worker = Worker(
         client, 
         task_queue="order-task-queue", 
-        workflows=[NotifyOrder], 
-        activities=[send_email]
+        workflows=[AbstractWorkflow]
     )
 
     # 运行 worker，这会使其开始监听任务队列并执行工作流和活动
